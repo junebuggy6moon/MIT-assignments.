9d22345de5e5204c68132afca6b6761a7759935d
Adam Ryan <58327211+AdamPatrickRyan@users.noreply.github.com>
2024-02-13 16:48:05 +0000

    Suggestion on validation and printing message

--- Problem_Set_2.py
+++ Problem_Set_2.py
@@ -24,6 +24,9 @@ while word_length != 0 and user_letter != "exit":
     print(" ".join(display_word))
     user_letter = input("Please guess a letter: ")
 
+    if len(user_letter.upper())>1:
+        print("You should only guess a single letter! Try again!")
+
     if user_letter.upper() in guessed_list:
         print("You had already guessed that letter! Try again!")
     else:
@@ -37,7 +40,7 @@ if user_letter == "exit":
     print("Thanks for playing! see you next time!")
 else:
     print(" ".join(user_letter))
-    print("Congratulations! You guessed the word!")
+    print(f"Congratulations! You guessed the word! It was {word}")
 
 
 
